dfd658b9e9cd5634dc14ea2db47a6bcab2180bb6
EC2 Default User <ec2-user@ip-172-31-67-249.ec2.internal>
2021-08-11 00:07:33 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='algogene',
+      version='1.0',
+      description='Bioinformatics mini library for manipulating sequences',
+      packages=['algogene'],
+      author='Callis Ezenwaka',
+      author_email='callisezenwaka@outlook.com',
+      zip_safe=False)

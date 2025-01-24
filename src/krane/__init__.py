1ea9374a111a5c494d37da10b07b8dbc71a6c82a
callezenwaka <callisezenwaka@gmail.com>
2025-01-24 11:43:23 +0000

    Change job name.

--- src/krane/__init__.py
+++ src/krane/__init__.py
@@ -1,8 +1,7 @@
 # src/krane/__init__.py
 from .core.sequence import Sequence  # Not .Sequence
 
-try:
-    from .version import version as __version__ # type: ignore
-except ImportError:
-    # If the version file doesn't exist (like during development)
-    __version__ = "0.0.0.dev0"
+# try:
+#     from .version import version as __version__ # type: ignore
+# except ImportError:
+#     __version__ = "0.0.0.dev0" # If the version file doesn't exist (like during development)
